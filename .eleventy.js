81c2c6624d45893139ed7a05c4bfcf8edebfd39b
pham-k <pham.k@protonmail.com>
2021-12-29 18:17:57 +0700

    Copy dist folder content to root dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addWatchTarget("./src/css");
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
     return {
         dir: {
