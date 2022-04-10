c864636883006578139125690c9b0990d69cf0ab
pham-k <pham.k@protonmail.com>
2022-04-10 11:52:31 +0700

    Version 2.0.0

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = eleventyConfig => {
+    eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addPassthroughCopy("./src/img/");
+
+    eleventyConfig.addFilter("randomPost", (arr) => {
+        arr.sort(() => {
+            return 0.5 - Math.random();
+        });
+        return arr.slice(0, 1);
+    })
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'build'
+        }
+    }
+}
